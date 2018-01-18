22e4bd1664d8f4a7c86e6d72c9b412f9098a4ffc
Abhiram Natarajan <abhiram.natarajan@gmail.com>
2018-01-18 23:33:17 +0530

    Updated SQL.java

--- SQL.java
+++ SQL.java
@@ -1,50 +1,62 @@
- import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SQL {
-    public static void DisplayQuery(HashMap<String ,ArrayList<String>> map){
-        ArrayList<String> al1,al2,al3,al4 = new ArrayList<String>();
-        al1=map.get("t_cstmrs");
-        System.out.println("t_cstmrs contains "+al1);
-        al1=map.get("t_colours");
+public class NLP {
 
-        System.out.println("t_colours contains"+al1);
+    public static void DisplayQuery(HashMap<String ,ArrayList<String>> map) {
+        ArrayList<String> al1, al2, al3, al4, al5 = new ArrayList<String>();
 
-        al1= map.get("t_grphy");
-        System.out.println("t_grphy contains "+al1);
+        al1 = map.get("t_cstmrs");
+        System.out.println("t_cstmrs contains   " + al1);
 
-        al1=map.get("t_prdcts");
-        System.out.println("t_prdcts contains "+al1);
-        // al2.clear();
+        al2 = map.get("t_clrs");
+        System.out.println("t_colours contains   " + al2);
+
+        al3 = map.get("t_grphy");
+        System.out.println("t_georaphy contains   " + al3);
+
+        al4 = map.get("t_clths");
+        System.out.println("t_clothes contains   " + al4);
+
+        al5 = map.get("t_sales");
+        System.out.println("t_sales contains   " + al5);
 
 
     }
+        public static void main(String[] args) {
+        HashMap<String,ArrayList<String>>m = new HashMap<String, ArrayList<String>>();
+            ArrayList<String> al1 = new ArrayList<String>();
+            ArrayList<String> al2 = new ArrayList<String>();
+            ArrayList<String> al3 = new ArrayList<String>();
+            ArrayList<String> al4 = new ArrayList<String>();
+            ArrayList<String> al5 = new ArrayList<String>();
 
-    public static void main(String[] args) {
-        HashMap<String,ArrayList<String>>hashmap = new HashMap<String, ArrayList<String>>();
-        ArrayList<String> al1 = new ArrayList<String>();
-        ArrayList<String> al2 = new ArrayList<String>();
-        ArrayList<String> al3 = new ArrayList<String>();
-        ArrayList<String> al4 = new ArrayList<String>();
-        String cust[]=new String[]{"Shubham","Tanmay","Yatish","Prathika","Shashi","Abhiram"};
+        String cust[]=new String[]{"Deepamala","Shobha","Chandra","Shubham","Tanmay","Yatish","Prathika","Shashi","Abhiram"};
         al1.addAll(Arrays.asList(cust));
+        m.put("t_cstmrs",al1);
 
-        hashmap.put("t_cstmrs",al1);
         String loc[]=new String[]{"India","Bangalore","Canada","Tokyo","California",
-                "Chicago","Tumkur","France","Germany"};
+                "Chicago","Korea","France","Germany"};
 
         al2.addAll(Arrays.asList(loc));
-        hashmap.put("t_grphy",al2);
+        m.put("t_grphy",al2);
 
-
-        String clrs[]=new String[] {"red","blue","yellow","black"};
+        String clrs[]=new String[] {"red","blue","yellow","black","pink","green","orange","purple","white"};
         al3.addAll(Arrays.asList(clrs));
-        hashmap.put("t_colours",al3);
-        String prdcts[]=new String[]{"mountain bikes","T-shirts","rolls royce phantom","colour pencils","playing cards"
-        ,"Hamburgers"};
-        al4.addAll(Arrays.asList((prdcts)));
-        hashmap.put("t_prdcts",al4);
-        DisplayQuery(hashmap);
+        m.put("t_clrs",al3);
 
-    }
+        String clths[]=new String[]{"shirt", "trousers", "jacket", "socks", "blouse", "t-shirt", "sweater", "pants", "shoes"};
+        al4.addAll(Arrays.asList(clths));
+        m.put("t_clths",al4);
+
+        String sales[]=new String[]{"$100,000", "$100,250", "$100,500", "$100,750", "$200,000", "$200,250", "$200,500", "$200,750", "$300,000"};
+        al5.addAll(Arrays.asList(sales));
+        m.put("t_sales",al5);
+
+        DisplayQuery(m);
+
+        }
 
 }
